65172d4a4e70527fa273a0e15eb2a492a16628df
Biblioteca Revolucionaria <xicamauro@protonmail.ch>
2022-12-16 20:12:51 -0300

    doc: added swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EligibilityModule } from './eligibility/eligibility.module';
 
 @Module({
-  imports: [],
+  imports: [EligibilityModule],
   controllers: [AppController],
   providers: [AppService],
 })
