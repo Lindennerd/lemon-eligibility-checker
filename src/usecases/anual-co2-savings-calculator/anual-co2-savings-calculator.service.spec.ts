b110849caff5e3189066b18a0de168e89f6e8bfb
Biblioteca Revolucionaria <xicamauro@protonmail.ch>
2022-12-17 01:12:53 -0300

    test: testing anual co2 savings

--- src/usecases/anual-co2-savings-calculator/anual-co2-savings-calculator.service.spec.ts
+++ src/usecases/anual-co2-savings-calculator/anual-co2-savings-calculator.service.spec.ts
@@ -17,4 +17,17 @@ describe('AnualCo2SavingsCalculatorService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  //! FAIR PLAY - é uma prova real da regra de tres
+  it('computed value should be decomposable', () => {
+    const test = [
+      3878, 9760, 5976, 2797, 2481, 5731, 7538, 4392, 7859, 4160, 6941, 4597,
+    ];
+
+    const expected = test.reduce((prev, curr) => prev + curr, 0);
+
+    const result = service.computeSavings(test);
+
+    expect(expected).toBe((result * 1000) / 84);
+  });
 });
