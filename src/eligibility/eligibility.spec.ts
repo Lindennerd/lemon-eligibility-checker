9be472237cb61a2e7c447b618f6b566d42958d19
Biblioteca Revolucionaria <xicamauro@protonmail.ch>
2022-12-16 23:39:14 -0300

    test: default tests passing

--- src/eligibility/eligibility.spec.ts
+++ src/eligibility/eligibility.spec.ts
@@ -1,4 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { AnualCo2SavingsCalculatorService } from 'src/usecases/anual-co2-savings-calculator/anual-co2-savings-calculator.service';
+import { EligibilityValidatorService } from 'src/usecases/eligibility-validator/eligibility-validator.service';
 import { Eligibility } from './eligibility';
 
 describe('Eligibility', () => {
@@ -6,7 +8,11 @@ describe('Eligibility', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [Eligibility],
+      providers: [
+        Eligibility,
+        EligibilityValidatorService,
+        AnualCo2SavingsCalculatorService,
+      ],
     }).compile();
 
     provider = module.get<Eligibility>(Eligibility);
