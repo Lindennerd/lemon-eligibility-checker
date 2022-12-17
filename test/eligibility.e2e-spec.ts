8bf9fb63a9f9bce42dee7cc02cec944fe9e06749
Biblioteca Revolucionaria <xicamauro@protonmail.ch>
2022-12-17 01:25:49 -0300

    refactor: remove unused file

--- test/eligibility.e2e-spec.ts
+++ test/eligibility.e2e-spec.ts
@@ -1,23 +1,23 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import { EligibilityModule } from 'src/eligibility/eligibility.module';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
 
-describe('AppController (e2e)', () => {
+describe('EligibilityController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [EligibilityModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('/check (POST)', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .post('/check')
       .expect(200)
       .expect('Hello World!');
   });
